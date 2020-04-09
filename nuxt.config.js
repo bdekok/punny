78b14f3e530ab6f9d489caab6a6b0bdc144b9cd8
Brent de Kok <brent@worth.systems>
2020-04-09 18:37:08 +0200

    Breaking on the favicon on github

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,9 +18,6 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
 
   /*
